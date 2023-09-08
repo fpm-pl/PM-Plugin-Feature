9ba63684e5fa0e0b39eff3d632f753ea80d936d9
nicholass003 <veganicholas123@gmail.com>
2023-09-08 21:28:30 +0700

    Better documentation

--- src/nicholass003/tester/Main.php
+++ src/nicholass003/tester/Main.php
@@ -6,19 +6,30 @@ namespace nicholass003\tester;
 
 use nicholass003\tester\commands\TesterCommand;
 use nicholass003\tester\events\PlayerEnchanting;
+use nicholass003\tester\tasks\Tick;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase
 {
     protected function onEnable() : void
     {
-        $this->getServer()->getCommandMap()->register("tester", new TesterCommand($this));
-        
         $this->registerTesterEvents();
+
+        $this->registerTesterTask();
+    }
+
+    public function registerTesterCommand() : void
+    {
+        $this->getServer()->getCommandMap()->register("tester", new TesterCommand($this));
     }
 
     public function registerTesterEvents() : void
     {
         $this->getServer()->getPluginManager()->registerEvents(new PlayerEnchanting(), $this);
+    }
+
+    public function registerTesterTask() : void
+    {
+        $this->getScheduler()->scheduleRepeatingTask(new Tick($this), 20); // task run every second, 20 = 1 second
     }
 }
